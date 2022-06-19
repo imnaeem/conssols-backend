a3d31e1ba7b04ee0e24c7e98ab08f85b96ffc7da
imnaeem <ceinfo.pk@gmail.com>
2022-06-19 21:09:33 +0500

    APIs url change

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // app.use("/company/profile", upload, CompanyProfile);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use("/company", companyRoutes);
 app.use("/client", clientRoutes);
 app.use("/admin", adminRoutes);
@@ -30,13 +34,9 @@ app.use("/admin", adminRoutes);
 app.use("/", homeRoutes);
 app.use("/user", authRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
