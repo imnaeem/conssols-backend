cad4af618fb22ac2a8bc1b9db37c01da96f23347
imnaeem <ceinfo.pk@gmail.com>
2022-06-19 17:03:55 +0500

    node start changes

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ import adminRoutes from "./routes/adminRoutes.js";
 import dotenv from "dotenv";
 
 const app = express();
+
 dotenv.config();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
@@ -62,6 +64,14 @@ app.use("/", homeRoutes);
 app.use("/user", authRoute);
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_direname, "client", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
