aedb33f05dafab1d7e954228e0c9ee3f70fc8fc3
imnaeem <ceinfo.pk@gmail.com>
2022-06-19 20:35:47 +0500

    APIs url change

--- index.js
+++ index.js
@@ -22,38 +22,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(__dirname + "/public"));
-
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images/");
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.originalname +
-        "_" +
-        // new Date().toLocaleDateString("es-CL") +
-        // Date.now() +
-        path.extname(file.originalname)
-    );
-  },
-});
-
-const imageFilter = function (req, file, cb) {
-  // Accept images only
-  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-    req.fileValidationError = "Only image files are allowed!";
-    return cb(new Error("Only image files are allowed!"), false);
-  }
-  cb(null, true);
-};
-
-const upload = multer({ storage: fileStorage, fileFilter: imageFilter }).single(
-  "profileImage"
-);
-
 // app.use("/company/profile", upload, CompanyProfile);
 app.use("/company", companyRoutes);
 app.use("/client", clientRoutes);
@@ -64,8 +32,6 @@ app.use("/user", authRoute);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  const path = require("path");
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
