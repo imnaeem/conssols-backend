aff5abdea3f1e4e47ebb35b2d6180bcc84238eaf
imnaeem <ceinfo.pk@gmail.com>
2022-06-19 22:09:57 +0500

    client in

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
