e5618a2ba5a7f2b4522425b1a8a3ac39f5dabd29
imnaeem <ceinfo.pk@gmail.com>
2022-06-19 22:06:08 +0500

    client in

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ import homeRoutes from "./routes/homeRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import dotenv from "dotenv";
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 dotenv.config();
