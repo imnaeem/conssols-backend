a3b470b79aa599757aa63b295a2adec041385f4f
imnaeem <ceinfo.pk@gmail.com>
2022-06-20 17:35:25 +0500

    Final

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
 
 // app.use("/company/profile", upload, CompanyProfile);
