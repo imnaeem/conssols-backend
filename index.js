ab62f48a279e3bb74b2d0326d779019b5d0918dd
imnaeem <ceinfo.pk@gmail.com>
2022-07-18 23:36:37 +0500

    Responsive

--- index.js
+++ index.js
@@ -17,22 +17,20 @@ import dotenv from "dotenv";
 const app = express();
 
 dotenv.config();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
 app.use(cors());
 
-// app.use("/company/profile", upload, CompanyProfile);
+// API Routes
 app.use("/api/company", companyRoutes);
 app.use("/api/client", clientRoutes);
 app.use("/api/admin", adminRoutes);
-
 app.use("/api", homeRoutes);
 app.use("/api/user", authRoute);
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// For Production Build
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
@@ -40,6 +38,8 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+// DB Connection
 
 const PORT = process.env.PORT || 5000;
 
