cd13d2b94efd7ace12e5bc1fada60fe204465def
imnaeem <ceinfo.pk@gmail.com>
2022-07-22 11:31:07 +0500

    Final

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,8 +16,9 @@ const auth = async (req, res, next) => {
 
     next();
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error.message });
+    res
+      .status(401)
+      .json({ message: "Sorry! You are not authorized to access this page" });
   }
 };
 
